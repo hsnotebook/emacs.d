b70b78eb52c94a311e8c26ba42a25a2179a6b462
hsnotebook <huangshuai010@gmail.com>
2015-11-12 17:17:40 +0800

    Expand region, binded to C-=.

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -11,5 +11,9 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+;; expand region
+(require-package 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
 
 (provide 'init-editing)

03cf8b9b7c8de2ffa3a07d09a157913c867b3a92
huangshuai <huangshuai010@gmail.com>
2015-11-13 16:55:06 +0800

    Configure emmet's key binding, and auto mode.

--- lisp/init-emmet.el
+++ lisp/init-emmet.el
@@ -1,5 +1,13 @@
 (require-package 'emmet-mode)
-(emmet-mode t)
+(require 'emmet-mode)
+
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+
+(eval-after-load 'emmet-mode
+  '(define-key emmet-mode-keymap (kbd "C-c C-h") 'emmet-prev-edit-point))
+(eval-after-load 'emmet-mode
+  '(define-key emmet-mode-keymap (kbd "C-c C-l") 'emmet-next-edit-point))
 
 
 (provide 'init-emmet)

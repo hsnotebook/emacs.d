454bfe59d3aac2e67fc26bfcb8b369db96a4ab40
huangshuai <huangshuai010@gmail.com>
2015-11-19 15:03:06 +0800

    Add emmet mode to xml.

--- lisp/init-emmet.el
+++ lisp/init-emmet.el
@@ -1,8 +1,11 @@
 (require-package 'emmet-mode)
 (require 'emmet-mode)
 
+;; html-mode's parent is sgml-mode,
+;; so just config for sgml-mode
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook 'emmet-mode)
+(add-hook 'nxml-mode-hook 'emmet-mode)
 
 (eval-after-load 'emmet-mode
   '(define-key emmet-mode-keymap (kbd "C-c C-h") 'emmet-prev-edit-point))

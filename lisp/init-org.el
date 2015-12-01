9f96d86736dcb5d83b1de850bcfe80c39d279669
huangshuai <huangshuai010@gmail.com>
2015-12-01 17:22:57 +0800

    Config org capture and agenda file.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,4 +1,10 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
+(setq org-agenda-files '("~/org"))
+
+(define-key global-map "\C-cc" 'org-capture)
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
+	 "* TODO %?\n  %i\n  %a")))
 
 
 (provide 'init-org)

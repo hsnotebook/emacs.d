416f469a1293958d104e7782906b9e8028f4f39c
huangshuai <huangshuai010@gmail.com>
2015-12-23 23:08:28 +0800

    Emacs Eclim is good!!

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -1,1 +1,15 @@
+(require-package 'emacs-eclim)
+(global-eclim-mode)
+
+(custom-set-variables
+ '(eclim-eclipse-dirs '("/home/hs/bin/eclipse"))
+ '(eclim-executable "/home/hs/bin/eclipse/eclim"))
+
+(setq help-at-pt-display-when-idle t)
+(help-at-pt-set-timer)
+
+(require 'ac-emacs-eclim-source)
+(ac-emacs-eclim-config)
+
+
 (provide 'init-java)

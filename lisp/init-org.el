9cc961be0e1b57c081f46d912c1faebca90ef3f4
huangshuai <huangshuai010@gmail.com>
2016-05-03 11:27:55 +0800

    Config org template and agenda files.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,10 +1,26 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
-(setq org-agenda-files '("~/org"))
 
 (define-key global-map "\C-cc" 'org-capture)
+
+(setq org-agenda-files (quote ("~/org-notes/")))
+(setq org-default-notes-file "~/org-notes/gtd.org")
+
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
-	 "* TODO %?\n  %i\n  %a")))
+      '(("l" "Life" entry (file+headline "~/org-notes/gtd.org" "Life")
+	 "* TODO [#B] %?\n  %i\n"
+	 :empty-lines 1)
+	("w" "Work" entry (file+headline "~/org-notes/gtd.org" "Work")
+	 "* TODO [#B] %?\n  %i\n"
+	 :empty-lines 1)
+	("n" "notes" entry (file+headline "~/org-notes/notes.org" "Quick notes")
+	 "* TODO [#C] %?\n  %i\n %U"
+	 :empty-lines 1)
+	("j" "Journal Entry"
+	 entry (file+datetree "~/org-notes/journal.org")
+	 "* %?"
+	 :empty-lines 1)))
+
+(require-package 'org-pomodoro)
 
 
 (provide 'init-org)

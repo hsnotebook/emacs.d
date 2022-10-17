d40269021b0720c5f27175c07258e6799f759d55
huangshuai <huangshuai@ctfo.com>
2022-10-17 18:04:42 +0800

    first commit

--- init.el
+++ init.el
@@ -1,32 +1,46 @@
 (setq inhibit-startup-message t)
 
+; ui
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
 (load-theme 'wombat)
+(add-to-list 'default-frame-alist
+             '(font . "Source Code Pro-13"))
 
-(require 'package)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(setq package-archives '(("gnu"   . "http://1.15.88.122/gnu/")
-			 ("org"   . "http://1.15.88.122/org/")
-                         ("melpa" . "http://1.15.88.122/melpa/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
+(straight-use-package 'use-package)
 (require 'use-package)
-(setq use-package-always-ensure t)
+(setq straight-use-package-by-default t)
+
+(use-package which-key
+  :config (which-key-mode))
+
+; evil
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode))
 
 (use-package evil
   :demand t
   :bind (("<escape>" . keyboard-escape-quit))
   :init (setq evil-want-keybinding nil)
-  :Config
-  (evil-mode 1)
+  :config
+  (evil-set-undo-system 'undo-tree)
+  (evil-mode 1))
 
 (use-package evil-collection
   :after evil
@@ -41,3 +55,14 @@
 (use-package evil-matchit
   :after evil
   :config (evil-matchit-mode))
+
+(use-package fcitx
+  :after evil
+  :config
+  (when (setq fcitx-remote-command
+	      (or (executable-find "fcitx5-remote")
+		  (executable-find "fcitx-remote")))
+    (fcitx-evil-turn-on)))
+
+;; git
+(use-package magit)
